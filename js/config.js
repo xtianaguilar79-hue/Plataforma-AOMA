5d00caf18a6ce0987f910e02348c514847f305ee
xtianaguilar79-hue <xtianaguilar79@gmail.com>
2026-07-14 01:01:02 -0300

    Add files via upload

--- js/config.js
+++ js/config.js
@@ -1,15 +1,16 @@
-// =========================================
-// CAMPUS AOMA
+//=====================================================
+// AOMA - Plataforma de Capacitación
 // config.js
-// =========================================
+//=====================================================
 
-// REEMPLAZAR LA ANON KEY POR LA TUYA
+// URL del proyecto Supabase
 const SUPABASE_URL = "https://kkgrqgdddizrmspsmqzy.supabase.co";
 
-const SUPABASE_ANON_KEY =
-"PEGA_AQUI_TU_ANON_KEY";
+// Clave pública (ANON KEY)
+const SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtrZ3JxZ2RkZGl6cm1zcHNtcXp5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODM5ODg0NjQsImV4cCI6MjA5OTU2NDQ2NH0.AhCJIA9toBoT5scduaCTR3ib-zFY71EV85eH65SPbys";
 
-const supabase = window.supabase.createClient(
+// Crear cliente de Supabase
+const supabaseClient = window.supabase.createClient(
     SUPABASE_URL,
     SUPABASE_ANON_KEY
 );
